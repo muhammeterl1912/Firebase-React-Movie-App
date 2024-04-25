f90982f919de4de33ed4998ee62667519a3f97cc
Muhammet Erol <muhammederol971@gmail.comgit config --global user.email muhammederol971@gmail.com>
2024-04-25 14:25:11 +0200

    Toastify methods has been created in the helpers

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import AppRouter from "./router/AppRouter";
 import AuthProvider from "./context/AuthProvider";
+import { ToastContainer } from "react-toastify";
 
 const App = () => {
   return (
     <div className="dark:bg-gray-dark-main min-h-screen">
       <AuthProvider>
         <AppRouter />
+        <ToastContainer/>
       </AuthProvider>
     </div>
   );
