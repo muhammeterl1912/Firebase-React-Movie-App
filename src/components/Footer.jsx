265d8164c697b53a5617ac824f0a834556d6c3d5
Muhammet Erol <muhammederol971@gmail.comgit config --global user.email muhammederol971@gmail.com>
2024-04-27 03:03:41 +0200

    finished repo.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,10 @@
 
 function Footer() {
+    const currentYear = new Date().getFullYear();
   return (
     <footer className='bg-red-main p-4 text-center  dark:text-white flex justify-between sm:text-sm'>
        <h1>Muhammet Erol</h1>
-      <p>&copy; 2024 Firesbse Movie App</p>
+      <p>&copy;  {currentYear} Firebase Movie App</p>
       <p>Powered by Firebase & React</p>
    
     </footer>
