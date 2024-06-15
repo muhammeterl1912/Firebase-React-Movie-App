60b178e4aa305dd5ed65df911eb34211896a301d
Muhammet Erol <muhammederol971@gmail.comgit config --global user.email muhammederol971@gmail.com>
2024-06-15 18:14:20 +0200

    development:Added useRef for performance.

--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -1,10 +1,10 @@
-import { useState } from "react";
+import { useRef } from "react";
 import { useMovieData } from "../context/MovieProvider";
 import { toastWarnNotify } from "../helper/ToastNotify";
 import { useContextAuth } from "../context/AuthProvider";
 
-
 const API_KEY = process.env.REACT_APP_TMDB_KEY;
+
 
 if (!API_KEY) {
   console.error("API key is missing! Please set REACT_APP_TMDB_KEY in your environment variables.");
@@ -13,18 +13,22 @@ if (!API_KEY) {
 const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`;
 
 const SearchInput = () => {
-  const [change, setChange] = useState("");
   const { fetchData } = useMovieData();
   const { currentUser } = useContextAuth();
+  const inputRef = useRef();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-   
-    if (change.trim() && currentUser) {
-      fetchData(SEARCH_API + change);
-      setChange(""); 
-    } else if (!currentUser) {
+    const searchValue = inputRef.current.value.trim();
+    
+    if (!currentUser) {
       toastWarnNotify("Please log in to search for a movie...");
+      return;
+    }
+
+    if (searchValue) {
+      fetchData(SEARCH_API + searchValue);
+      inputRef.current.value = ""; 
     } else {
       toastWarnNotify("Please enter a text to search for a movie.");
     }
@@ -37,8 +41,7 @@ const SearchInput = () => {
           type="search"
           className="w-80 h-8 rounded-md p-1 m-2"
           placeholder="Search for a movie..."
-          value={change} 
-          onChange={(e) => setChange(e.target.value)}
+          ref={inputRef}
         />
         <button className="btn-danger-bordered uppercase">Search</button>
       </form>
